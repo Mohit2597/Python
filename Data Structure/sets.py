5cc644a0446e038b6d170ee4d3690dc0c4b73af6
Mohit2597 <monty.chaudhary94@icloud.com>
2021-03-05 03:50:21 +0530

    P-Day1

---
+++ Data Structure/sets.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+my_set={1,2,3,4,5,6,6,5,3}
+
+# **** IMPORTANT ****
+#   it will order you data and will remove the duplicates
+#
+#   empty_set={{}}
+#   with 2 curly braces if you will use one curly brace it will be an empty dictionary
+#
+#   list=[1,2,3,4,5,6,7]
+#   set={list}
+#   you can convert list into a set
+
+
+#   print(my_set)
+#   print(bool(my_set))
+#   print(type(my_set))
+#   dir(Set)
+#   my_set.union(my_empty_set)                                  same like maths union of sets
+#   my_set.intersection(my_empty_Set)                           same like maths intersection of sets
+
+
+#@ lacture 4
---
+++ operators/arithmatic.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#     ********** IMPORTANT **********
+'''
+    addition            +
+    subtraction         -
+    division            /
+    multipliction       *
+    modulo              %
+    Floor division      //     2/7 = 3 
+    exponational        **     2 power 4
+
+
+'''
