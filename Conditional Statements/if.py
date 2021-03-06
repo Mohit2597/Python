0e35d99e1f65fa9970b1e6c6bd7cb71c20ad26cd
Mohit2597 <monty.chaudhary94@icloud.com>
2021-03-07 00:08:42 +0530

    day2

--- Conditional Statements/if.py
+++ Conditional Statements/if.py
@@ -1,1 +1,36 @@
 #!/usr/bin/env python3
+
+#-->if ture condition:
+#   and in condition you can use operators
+
+#-->if true condition:
+#       statement
+#   elif ture condition:
+#       statement
+#   elif true condition:
+#       statement
+
+
+'''
+user_string=input('Enter your Name: ')
+print(f'Name: {user_string}')
+user_cnf=input('Do you want to print this name "Yes" or "No": ')
+
+if user_cnf == "Yes":
+    print(user_string.title())
+else:
+    print(user_string)
+'''
+
+num1=eval(input('Enter number 1: '))
+num2=eval(input('Enter number 2: '))
+
+if num1>num2:
+    print(num1)
+elif num1<num2:
+    print(num2)
+elif num1==num2:
+    print(num1)
+
+
+#@lature1
