ae24fbfa94758e9a242f56e44568832bf79c7d3a
Mohit2597 <monty.chaudhary94@icloud.com>
2021-03-06 10:08:47 +0530

    day2

--- operators/arithmatic.py
+++ operators/arithmatic.py
@@ -7,8 +7,15 @@
     division            /
     multipliction       *
     modulo              %
-    Floor division      //     2/7 = 3 
+    Floor division      //     2/7 = 3
     exponational        **     2 power 4
 
 
+'''
+x=a+b a+=b
+a-=b
+a/=b
+a*=b
+a%=b
+a=b
 '''
